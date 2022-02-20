50b7b5b80563522ad96950bab0aea5881bd85b4c
nguyetpham09 <nguyetpham969@gmail.com>
2022-02-20 21:09:10 +0700

    first commit

---
+++ TeduShop.Model/Models/OrderInformation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Models
+{
+    public class OrderInformation
+    {
+        public int OrderId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerPhoneNumber { get; set; }
+        public string CustomerAddress { get; set; }
+        public string PaymentMethod { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public bool Status { get; set; }
+        public string PaymentStatus { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public string CustomerId { get; set; }
+    }
+}
---
+++ TeduShop.Web/Infrastructure/GoShipAPI/GoShipCity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeduShop.Web.Infrastructure.GoShipAPI
+{
+    public class GoShipCity
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+}
